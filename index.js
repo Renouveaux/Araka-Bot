eb141558dd71f8a8340a8774c61d06acaee43be9
Renouveaux <renouveaux@hotmail.com>
2016-09-04 00:21:52 +0200

    Start poll

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 var cp = require('child_process');
 var bot = cp.fork(`${__dirname}/bot/index.js`);
+
+var config = require('./config-file.json')
+, express = require('express')
+, cors = require('cors')
+, io = require('socket.io')
+, app = express();
+
+app.use(cors());
+app.use(express.static(__dirname + '/web'));
+
+var server = require('http').createServer(app)
+, server = require('http').createServer(app)
+, io = io.listen(server);
+
+server.listen(config.port);
